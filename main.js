269d9d2b0124050267cb2e52d117f01a87001a47
karolwalasik <karol.walasik@gmail.com>
2019-01-18 14:14:24 +0100

    zmiana klasy

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     let menu = document.getElementById("toggleButton");
-    document.getElementById("menu").classList.remove("showmenu");
+    document.getElementById("menu").classList.add("hidemenu");
     menu.addEventListener("click", function () {
-        document.getElementById("menu").classList.toggle("showmenu");
+        document.getElementById("menu").classList.toggle("hidemenu");
 
     });
 }, false);
